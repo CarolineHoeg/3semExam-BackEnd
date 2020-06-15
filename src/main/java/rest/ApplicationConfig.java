30292036a34574013c762593f1598c2db5152bde
Caroline Hoeg <carolinehoeg123@gmail.com>
2020-06-15 09:52:38 +0200

    cleaned up startcode

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -25,7 +25,6 @@ public class ApplicationConfig extends Application {
         resources.add(errorhandling.AuthenticationExceptionMapper.class);
         resources.add(errorhandling.GenericExceptionMapper.class);
         resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
-        resources.add(rest.FetchDemoResource.class);
         resources.add(rest.RenameMeResource.class);
         resources.add(rest.RoleDemoResource.class);
         resources.add(security.JWTAuthenticationFilter.class);
